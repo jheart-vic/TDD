6f6562b910a4e314b221264db5a830207596c3b8
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-02 12:05:31 +0100

    Add test for Factorial method

---
+++ spec/solver.spec.rb
@@ -0,0 +1,18 @@
+require './solver'
+
+describe Solver do
+  context 'Test Solver class and it method' do
+    it 'factorial -1 to be false'do
+    solver = Solver.new()
+    expect(solver.factorial(0)).to be false
+  end
+    it 'factorial 0 to be 1'do
+    solver = Solver.new()
+    expect(solver.factorial(0)).to be 1
+  end
+    it 'factorial 5 to be 120'do
+    solver = Solver.new()
+    expect(solver.factorial(0)).to be 120
+  end
+  end
+end
