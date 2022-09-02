7da335f967c4e64b6d8beccf225a3a207698d309
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-02 15:18:38 +0100

    Add fizzbuzz method to pass test

--- solver.rb
+++ solver.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # the solver class
 class Solver
   def factorial(num)
@@ -10,5 +8,12 @@ class Solver
   end
   def reverse_string(str)
     str.reverse
+  end
+
+  def fizz_buzz(number)
+    return number if number % 3 != 0 && number % 5 != 0
+    return 'fizzbuzz' if (number % 15).zero?
+    return 'fizz' if (number % 3).zero?
+    return 'buzz' if (number % 5).zero?
   end
 end
